1fc6ea6268dd4459bfece29e874bbed400d23083
ciordan <Catalin.Iordan@endava.com>
2020-10-28 22:01:35 +0200

    Initial commit

---
+++ src/main/java/com/icode/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.icode.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
